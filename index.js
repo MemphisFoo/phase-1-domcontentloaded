4fc0c566001ca3434bd8b0108a3c418dd1b2d8b1
Christian Trublet de Nermont <cjwtdn@gmail.com>
2022-06-30 10:00:07 -0600

    commit tree 90d273c141a8f18b203d0d5319e8200243c846c8

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+const loaded = document.getElementById('text');
+
+document.addEventListener('DOMContentLoaded', function() {
+    updateDOM();
+});
+
+function updateDOM() {
+    loaded.textContent = "This is really cool!";
+}
